81dedc42b0fffc1c77f19a5c30042a30c77ed60f
Fedor Yakouchyk <fedar77@outlook.com>
2023-03-14 23:31:55 +0300

    task / добавлен метод для создания исходного массива

--- task/Program.cs
+++ task/Program.cs
@@ -2,3 +2,24 @@
 {
     System.Console.WriteLine("['" + String.Join("', '", array) + "']");
 }
+string[] NewArr()
+{
+    System.Console.WriteLine("Введите размер массива.");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string [size];
+    if (size > 0)
+    {
+        System.Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Console.ReadLine();
+        }
+        PrintArray(array);
+    }
+    else
+    {
+        System.Console.WriteLine("Введённое вам значение не корректно, попробуйте снова!");
+
+    }
+    return array;
+}
